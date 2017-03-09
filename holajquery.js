9af0710ab0d82cacafaf93cbc31c1faa677688bf
Bernardo Bernal <supermasterspark100@gmail.com>
2017-03-09 12:58:23 -0600

    se agrego ejemplos de eventos en jQuery

--- holajquery.js
+++ holajquery.js
@@ -4,11 +4,21 @@
 // {
 // alert($('h1').text())
 // }
-$(function()
- {
-    //alert($('h1').text()) 
-    //se crea un parrafo nuevo 
-    //en un objeto jQuery
-    $nuevo  = $('<p>Parrafo nuevo</p>')
-    $('h2').after($nuevo)
- })
+// $(function()
+//  {
+//     //alert($('h1').text()) 
+//     //se crea un parrafo nuevo 
+//     //en un objeto jQuery
+//     $nuevo  = $('<p>Parrafo nuevo</p>')
+//     $('h2').after($nuevo)
+//  })
+
+// $('#boton1').click(function (e) { 
+//     e.preventDefault();
+    
+    
+// });
+
+//  $(selector).on(events, function () {
+     
+//  });
